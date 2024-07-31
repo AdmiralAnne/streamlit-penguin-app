6a6a744941536fe99eed74dbeb6153476a2088f3
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-07-31 15:14:51 +0530

    changed st.write to st.info

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 # giving a title to the application
 st.title('🐧 Penguin Species Prediction App 🐧')
 
-st.write('This is a simple Machine Learning app that predicts species of a penguin based on key features.')
+st.info('This is a simple Machine Learning app that predicts species of a penguin based on key features.')

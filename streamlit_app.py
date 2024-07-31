9196bf4a1dd5aa09aae1ceeadfed7f53160f1a9a
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-07-31 15:19:13 +0530

    imported libraries and added a dataset of Penguins.csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+
 # giving a title to the application
 st.title('🐧 Penguin Species Prediction App 🐧')
 
 st.info('This is a simple Machine Learning app that predicts species of a penguin based on key features.')
+# use st.write if you want to write normal texts instead of having the blue background
+
+# create a dataframe that stores the penguin data csv file -> already cleaned
+df = pd.read_csv("https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv")
+df

9c6d8a62267ccfbd5645cbfc661cd55382e64a1f
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-07-31 15:31:53 +0530

    added comments and x dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,17 @@ st.info('This is a simple Machine Learning app that predicts species of a pengui
 # use st.write if you want to write normal texts instead of having the blue background
 
 # add an expander -> Like a toggle button that expands upon clicking
-with st.expander('Data'):
+with st.expander('**Data**'):
     # add a title for this dataframe that's being displayed
     st.write('Raw Data csv')
     # create a dataframe that stores the penguin data csv file -> already cleaned
     df = pd.read_csv("penguins_cleaned.csv")
     df
+
+    # divide the data into x and y -> features vs Label
+    # lets make x first -> which is the dataset without the species feature
+    st.write(**feature_set**)
+    # create a variable x that stores the featurset
+    X = df.drop['species',axis=1]
+    X
+    # y will be just the species feature column i.e our label

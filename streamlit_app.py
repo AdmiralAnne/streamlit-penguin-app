549c97a231a32b91611a4d0a66625e3270c42903
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-07-31 15:26:19 +0530

    added expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,10 @@ st.title('🐧 Penguin Species Prediction App 🐧')
 st.info('This is a simple Machine Learning app that predicts species of a penguin based on key features.')
 # use st.write if you want to write normal texts instead of having the blue background
 
-# create a dataframe that stores the penguin data csv file -> already cleaned
-df = pd.read_csv("penguins_cleaned.csv")
-df
+# add an expander -> Like a toggle button that expands upon clicking
+with st.expander('Data'):
+    # add a title for this dataframe that's being displayed
+    st.write('Raw Data csv')
+    # create a dataframe that stores the penguin data csv file -> already cleaned
+    df = pd.read_csv("penguins_cleaned.csv")
+    df
